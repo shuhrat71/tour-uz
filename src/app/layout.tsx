f6576507819f99718667514702fd8924f455ebcc
shuhrat71 <toshxujayivshuhrat7@gmail.com>
2025-05-17 19:34:27 +0500

    add global styles and new dependencies for improved layout and functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import type { Metadata } from "next";
 import { Footer, Header } from "@/components";
 import { Roboto } from "next/font/google";
+import "./globals.css";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "../theme";
 
